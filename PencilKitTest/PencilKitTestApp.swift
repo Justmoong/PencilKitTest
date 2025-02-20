1be5332d2565b8ae3083ff87244fcbc626ac7e74
Justmoong <andudyun0504@gmail.com>
2025-02-20 17:18:55 +0900

    Initial Commit

---
+++ PencilKitTest/PencilKitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PencilKitTestApp.swift
+//  PencilKitTest
+//
+//  Created by ymy on 2/20/25.
+//
+
+import SwiftUI
+
+@main
+struct PencilKitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
